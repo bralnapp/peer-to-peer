9b033fb479d241613b659d8e0772c59e2c921d39
coolpythoncodes <coolpythoncodes@gmail.com>
2022-09-15 09:42:36 +0100

    changed socket server url

--- src/hooks/useChat.js
+++ src/hooks/useChat.js
@@ -2,15 +2,13 @@ import { useEffect, useRef, useState } from "react";
 import socketIOClient from "socket.io-client";
 
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage"; // Name of the event
-// const SOCKET_SERVER_URL = "https://radenu-chat-dapp.herokuapp.com/:4000";
-const SOCKET_SERVER_URL = "http://localhost:4000";
+const SOCKET_SERVER_URL = "https://radenu-chat-dapp.herokuapp.com/:4000";
 
 const useChat = (roomId) => {
   const [messages, setMessages] = useState([]); // Sent and received messages
   const socketRef = useRef();
 
   useEffect(() => {
-
     // Creates a WebSocket connection
     socketRef.current = socketIOClient(SOCKET_SERVER_URL, {
       // transports : ['websocket'],
