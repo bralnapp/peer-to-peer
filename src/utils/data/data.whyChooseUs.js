16087ef55125f5cd2559878c7e96ed6f980ad123
coolpythoncodes <coolpythoncodes@gmail.com>
2022-09-15 16:33:53 +0100

    updated the landing page

--- src/utils/data/data.whyChooseUs.js
+++ src/utils/data/data.whyChooseUs.js
@@ -1,16 +1,21 @@
+import IconOne from "src/assests/choose1.png"
+import IconTwo from "src/assests/choose2.png"
+import IconThree from "src/assests/choose3.png"
+
+
 export const whyChooseUs = [
     {
-        icon: '',
+        icon: IconOne,
         heading: "decentralized",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis vitae ligula gravida mauris, lectus. Penatibus enim mattis sit a. ",
     },
     {
-        icon: '',
+        icon: IconTwo,
         heading: "fast",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis vitae ligula gravida mauris, lectus. Penatibus enim mattis sit a. ",
     },
     {
-        icon: '',
+        icon: IconThree,
         heading: "Secure",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis vitae ligula gravida mauris, lectus. Penatibus enim mattis sit a. ",
     },
